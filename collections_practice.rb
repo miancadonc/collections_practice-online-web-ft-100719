47ffc5e5215b9d71f777999f569935599436c6f1
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-13 03:32:32 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,10 +10,11 @@ def sort_array_char_count(array)
   array.sort {|a,b| a.length <=> b.length}
 end
 
-def swap_elements(array)
-  elem2 = array[1]
-  elem3 = array[2]
-  array[1] = elem3
-  array[2] = elem2
+def swap_elements(array, index, destination_index)
+  elem1 = array[index]
+  elem2 = array[destination_index]
+  array[index] = elem2
+  array[destination_index] = elem2
   array
 end
+
